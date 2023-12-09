a5961d8adba6147f52a49d7a59363a02dd872d5c
jvcmtr <jvictorcmtr@gmail.com>
2023-12-09 19:36:56 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+simple project trying to solve Moser's Circle Problem 
+i saw it on a 3blue1brown youtube channel and tried to solve it on paper. i struggle a little to express the recurciveness of the function in mathematical terms, so i decided to solve it on code.  
+
+The problem: if you define N points in a circle and draw lines connecting all of them, you devided the circle in how many regions
+    the problem: https://www.youtube.com/shorts/VFbyGEZLMZw
+    the solution: https://www.youtube.com/watch?v=YtkIWDE36qU
---
+++ circleDivision.py
@@ -0,0 +1,27 @@
+def solveForPoints(n_points):
+    if n_points <= 1:
+        return 1
+    
+    prev = solveForPoints(n_points-1)
+    return prev + getExtraRegions(n_points)
+
+
+def getExtraRegions(nth_point):
+    on_right = nth_point - 2
+    on_left = 0
+    extraRegions = 0
+
+    while(on_right >= 0):
+        extraRegions += drawLine(on_left, on_right)
+        on_left += 1
+        on_right -= 1
+    return extraRegions
+        
+
+def drawLine(left, right):
+    return (left*right)+1
+
+for i in range(1, 20):
+    print(f"{i} : {solveForPoints(i)}")
+
+    
